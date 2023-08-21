c83f327bb3e250227b340938df2e11b82038bc20
omartinez2020b <o.martinez.2020@alumnos.urjc.es>
2023-08-21 16:35:15 +0200

    Creating pkg

--- src/webbt_ws/src/py_gardener/setup.py
+++ src/webbt_ws/src/py_gardener/setup.py
@@ -11,26 +11,17 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/demo.launch.py']),
-        ('share/' + package_name + '/resource', [
-            'resource/tree.xml',
-        ]),
         ('share/' + package_name + '/py_gardener', [
             'py_gardener/parser.py',
             'py_gardener/translator.py',
-        ]),
-        ('share/' + package_name + '/actions', [
-            'actions/Forward.py',
-            'actions/Backward.py',
-            'actions/Turn.py',
-            'actions/CheckObstacle.py'
+            'py_gardener/garden_tools.py'
         ]),
     ],
     install_requires=['setuptools', 'py_trees', 'py_trees_ros', 'std_msgs', 'geometry_msgs', 'sensor_msgs'],
     zip_safe=True,
     author='Óscar Martínez',
     author_email='oscar.robotics@tutanota.com',
-    maintainer='Your Name',
+    maintainer='Óscar Martínez',
     maintainer_email='oscar.robotics@tutanota.com',
     keywords=['ROS2', 'py_trees'],
     classifiers=[
@@ -39,7 +30,7 @@ setup(
         'Programming Language :: Python',
         'Topic :: Software Development',
     ],
-    description='A execution engine BT.cpp trees in python',
+    description='An translator to achieve BT.cpp behavior with py_trees',
     license='BSD',
     tests_require=['pytest'],
     entry_points={
